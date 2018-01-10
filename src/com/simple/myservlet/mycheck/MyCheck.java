3e0f11890b9593c30f505306370b3ed5d3c3c329
gitghought <732475885@qq.com>
2018-01-10 11:21:34 +0800

    实现绘制字符串到图片“

--- src/com/simple/myservlet/mycheck/MyCheck.java
+++ src/com/simple/myservlet/mycheck/MyCheck.java
@@ -17,36 +17,49 @@ public class MyCheck extends HttpServlet {
 
 	/**
 	 * The doGet method of the servlet. <br>
-	 *
+	 * 
 	 * This method is called when a form has its tag value method equals to get.
 	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		BufferedImage bi = new BufferedImage(200, 100, BufferedImage.TYPE_INT_RGB);
-		
+		BufferedImage bi = new BufferedImage(1000, 500,
+				BufferedImage.TYPE_INT_RGB);
+
 		Graphics gra = bi.getGraphics();
 		gra.setColor(Color.red);
-		gra.drawRect(0, 0, 100, 50);
-		
+		gra.drawRect(0, 0, 1000, 500);
+
+		String str = "good";
+		gra.drawString(str, 100, 100);
+
 		ImageIO.write(bi, "png", response.getOutputStream());
 
 	}
 
 	/**
 	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
 	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
